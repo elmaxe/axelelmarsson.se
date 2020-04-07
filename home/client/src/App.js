248c146a5a2aceca6e34999c95a8a680138515d6
Axel Elmarsson <axel.elmarsson@gmail.com>
2020-04-07 09:20:02 +0200

    Add background particles

--- home/client/src/App.js
+++ home/client/src/App.js
@@ -7,61 +7,67 @@ import LinkedInIcon from './images/linkedin-brands.svg';
 import CVIcon from './images/file-alt-regular.svg';
 import PhoneIcon from './images/phone-square-alt-solid.svg';
 
+import Particles from 'react-particles-js';
+import reactParticlesConfig from './reactParticlesConfig'
+
 const App = () => {
     return (
-        <div className="Page">
-            <div className="AXELRUTA">
-                <img src={Island2019} alt="Axel Elmarsson" className="AXEL" title="Nämen hallå! Denna bild är tagen på Island sommaren 2019. Foto: Jesper Sjöholm"/>
-            </div>
-            <div>
-                <h1>Axel Elmarsson</h1>
-            </div>
-            <div className="Metatext">
-                Civilingenjörsstudent i datateknik på KTH.
-            </div>
-            <div className="Bar">
-                    <p>
-                        <a
-                            href="mailto:axel.elmarsson@gmail.com"
-                            title="Mejla mig om du vill, men föredrar brev! /s"
-                            >
-                            <img src={MailIcon} alt="E-mail" className="Images"/>
-                        </a>
-                        <a
-                            href="https://github.com/elmaxe"
-                            title="Min Github :O"
-                            rel="noopener noreferrer" target="_blank"
-                            >
-                            <img src={GithubIcon} alt="Github logo" className="Images"/>
-                        </a>
-                        <a
-                            href="/public/CV_AXEL_ELMARSSON.pdf"
-                            title="Mitt CV..."
-                            rel="noopener noreferrer" target="_blank"
-                            >
-                            <img src={CVIcon} alt="CV" className="Images CV"/>
-                        </a>
-                        <a
-                            href="https://www.linkedin.com/in/axel-elmarsson-453a93159/"
-                            title="Lägg till mig på LinkedIn!"
-                            rel="noopener noreferrer" target="_blank"
-                            >
-                            <img src={LinkedInIcon} alt="LinkedIn logo" className="Images"/>
-                        </a>
-                        <a 
-                            href="tel:+46767901038"
-                            title="Ring mig inte pls!"
-                            >
-                            <img src={PhoneIcon} alt="Phone" className="Images"/>
-                        </a>
+        <div>
+            <Particles className="particles" width="99vw" height="99vh" params={reactParticlesConfig}/>
+            <div className="Page">
+                <div className="AXELRUTA">
+                    <img src={Island2019} alt="Axel Elmarsson" className="AXEL" title="Nämen hallå! Denna bild är tagen på Island sommaren 2019. Foto: Jesper Sjöholm"/>
+                </div>
+                <div>
+                    <h1>Axel Elmarsson</h1>
+                </div>
+                <div className="Metatext">
+                    Civilingenjörsstudent i datateknik på KTH.
+                </div>
+                <div className="Bar">
+                        <p>
+                            <a
+                                href="mailto:axel.elmarsson@gmail.com"
+                                title="Mejla mig om du vill, men föredrar brev! /s"
+                                >
+                                <img src={MailIcon} alt="E-mail" className="Images"/>
+                            </a>
+                            <a
+                                href="https://github.com/elmaxe"
+                                title="Min Github :O"
+                                rel="noopener noreferrer" target="_blank"
+                                >
+                                <img src={GithubIcon} alt="Github logo" className="Images"/>
+                            </a>
+                            <a
+                                href="/public/CV_AXEL_ELMARSSON.pdf"
+                                title="Mitt CV..."
+                                rel="noopener noreferrer" target="_blank"
+                                >
+                                <img src={CVIcon} alt="CV" className="Images CV"/>
+                            </a>
+                            <a
+                                href="https://www.linkedin.com/in/axel-elmarsson-453a93159/"
+                                title="Lägg till mig på LinkedIn!"
+                                rel="noopener noreferrer" target="_blank"
+                                >
+                                <img src={LinkedInIcon} alt="LinkedIn logo" className="Images"/>
+                            </a>
+                            <a 
+                                href="tel:+46767901038"
+                                title="Ring mig inte pls!"
+                                >
+                                <img src={PhoneIcon} alt="Phone" className="Images"/>
+                            </a>
 
-                    </p>
-            </div>
-            <div className="TextLink">
-                <a href="#" title="Under konstruktion">Projekt</a>
-            </div>
-            <div className="Fötter">
-                Skrivet i React.js av Axel Elmarsson 2020
+                        </p>
+                </div>
+                <div className="TextLink">
+                    <a href="#" title="Under konstruktion">Projekt</a>
+                </div>
+                <div className="Fötter">
+                    Skrivet i React.js av Axel Elmarsson 2020
+                </div>
             </div>
         </div>
     )
